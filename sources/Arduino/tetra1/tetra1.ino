bc7a752715e3f7068e3ffd5fc7d8897cf657acf8
Zubair Iftikhar <zubair91@hotmail.fr>
2012-05-15 16:00:51 +0200

    test {BMP,RHT} ok sur Mini1 (sans translateur pour le BMP), cf l'exemple output.txt

--- sources/Arduino/tetra1/tetra1.ino
+++ sources/Arduino/tetra1/tetra1.ino
@@ -1,7 +1,7 @@
 /*
 Programme à embarquer sur l'Arduino n°1 du τετραφάρμακος.
 TODO
--stériliser le code;
+-ajouter la µCAM n°1
 -ajouter les headers définis par Denis pour signer chaque type de données et faciliter la lecture;
 -ajouter la commutation du module XBee avec la Arduino n°2.
 */
@@ -9,36 +9,56 @@ TODO
 #include <Wire.h>
 #include <SoftwareSerial.h>
 #include "BMP085.h"
-#include "DHT22.h"
+#include "DHT.h"
 
-//BMP085
-BMP085 captTP;
-
-//DH22
-DHT22 captHT(9);
+//RHT03
+#define DHTPIN 2
+#define DHTTYPE DHT22 
+DHT dht(DHTPIN, DHTTYPE);
 
 
 void setup(){
-	Serial.begin(115200);
-
-        //BMP085
-	captTP.begin();
+  Serial.begin(115200);
+  
+  //BMP085
+  Serial.println("BMP085");
+  Wire.begin();
+  bmp085Calibration();
+  
+  //RHT03
+  Serial.println("RHT03");
+  dht.begin();
 }
 
 void loop(){
 
-	/*
-	Sortie série de la pression et de la température du capteur BMP085
-	*/
-	Serial.print(captTP.getTemperature());
-	Serial.print(" // ");
-	Serial.println(captTP.getPressure());
-	
-	/*
-	Sortie série de l'humidité et de la température du capteur DH22
-	*/
-	Serial.print(captHT.getHumidity());
-	Serial.print(" // ");
-	Serial.println(captHT.getTemperatureC());
+  /*
+  Sortie série de la pression et de la température du capteur BMP085
+  */
+  float temperature = bmp085GetTemperature(bmp085ReadUT()); //MUST be called first
+  float pressure = bmp085GetPressure(bmp085ReadUP());
+  Serial.print("BMP085: \t T: ");
+  Serial.print(temperature, 2); //display 2 decimal places
+  Serial.print(" deg C\t");
+  Serial.print("P: ");
+  Serial.print(pressure, 0); //whole number only.
+  Serial.println(" Pa");
 
+  /*
+  Sortie série de l'humidité et de la température du capteur DH22
+  */
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // check if returns are valid, if they are NaN (not a number) then something went wrong!
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
+  }
+  else {
+    Serial.print("RHT03: \t Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C");
+  }
 }
