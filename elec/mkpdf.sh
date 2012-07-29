6660eac9b2cc70a482957761880d3dfcc16765b2
Zubair Iftikhar <zubair91@hotmail.fr>
2012-07-29 13:19:31 +0200

    petit nettoyage

--- elec/mkpdf.sh
+++ elec/mkpdf.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 echo "Printing PDF file from PS file"
 START=`date +%s%N`
-#startnum=${#deb}
 ps2pdf -sPAPERSIZE=a4 tetrapharmakos.ps
 END=`date +%s%N`
-#endnum=${#end}
 TIME=$(( ($END-$START)/1000000 ))
 echo "Done, in ~"$TIME "µs."
