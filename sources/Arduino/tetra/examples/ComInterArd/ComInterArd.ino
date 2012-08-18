729ceed87578ecf6dc87403640bf5642cb1b3444
Zubair Iftikhar <zubair91@hotmail.fr>
2012-08-18 18:58:17 +0200

    Programme de test de communication via SoftwareSerial entre 2 Arduino, OK, mais peut être améliorée (trop lent et trop d'erreurs).

---
+++ sources/Arduino/tetra/examples/ComInterArd/ComInterArd.ino
@@ -0,0 +1,28 @@
+/*
+Programme de test de communication entre deux Arduino via une connection en SoftwareSerial.
+Il y a 4 fils connectés entre les 2 Arduinos.
+
+Il faut embarquer le programme ComInterArd2.ino sur l'autre Arduino.
+
+Le résultat n'est pas terrible (erreurs fréquentes et lenteur). Il faut sûrement lire des packets d'octets avant de les envoyer.
+*/
+
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial InterArduino(6, 7); // RX, TX
+
+void setup() {
+	Serial.begin(9600);
+	Serial.println("Debut");
+
+	InterArduino.begin(9600);
+	InterArduino.println("Debut Soft");
+}
+
+void loop() {
+	InterArduino.println("Arduino1");
+	if (InterArduino.available())
+		Serial.write(InterArduino.read());
+	delay(1000);
+}
