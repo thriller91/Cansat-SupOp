eaddea514aa7171e40370533f3e6968a5ddb7eb7
Zubair Iftikhar <zubair91@hotmail.fr>
2012-08-19 21:57:32 +0200

    Réception d'un truc bizzare (aucune information, mais taille de fichier du bon ordre de grandeur)

--- sources/Arduino/tetra/examples/ComInterArd/ComInterArd.ino
+++ sources/Arduino/tetra/examples/ComInterArd/ComInterArd.ino
@@ -7,8 +7,10 @@ Il faut embarquer le programme ComInterArd2.ino sur l'autre Arduino.
 Le résultat n'est pas terrible (erreurs fréquentes et lenteur). Il faut sûrement lire des packets d'octets avant de les envoyer.
 */
 
-
+#include <SD.h>
 #include <SoftwareSerial.h>
+
+#define CHIP_SELECT 10
 
 SoftwareSerial InterArduino(6, 7); // RX, TX
 
@@ -17,12 +19,40 @@ void setup() {
 	Serial.println("Debut");
 
 	InterArduino.begin(9600);
-	InterArduino.println("Debut Soft");
+	pinMode(4, OUTPUT);
+	pinMode(5, INPUT);
+	digitalWrite(4, LOW);
+
+	Serial.print("Initializing SD card...");
+	pinMode(10, OUTPUT);
+
+	if (!SD.begin(CHIP_SELECT)) {
+		Serial.println("Card failed, or not present");
+		return;
+	}
+	Serial.println("card initialized.");
+
+	File dataFile = SD.open("IMG01.JPG");
+
+	delay(5000);
+	digitalWrite(4, HIGH);
+	if (dataFile) {
+		while (dataFile.available())
+			InterArduino.write(dataFile.read());
+	}
+
+	else {
+		Serial.println("error opening file to send");
+	}
+
+	dataFile.close();
+	digitalWrite(4, LOW);
+	while(digitalRead(5) == HIGH) {
+	}
+
 }
 
 void loop() {
-	InterArduino.println("Arduino1");
-	if (InterArduino.available())
-		Serial.write(InterArduino.read());
+	Serial.println("Fin");
 	delay(1000);
 }
--- sources/Arduino/tetra/examples/ComInterArd2/ComInterArd2.ino
+++ sources/Arduino/tetra/examples/ComInterArd2/ComInterArd2.ino
@@ -7,22 +7,75 @@ Il faut embarquer le programme ComInterArd.ino sur l'autre Arduino.
 Le résultat n'est pas terrible (erreurs fréquentes et lenteur). Il faut sûrement lire des packets d'octets avant de les envoyer.
 */
 
-
+#include <SD.h>
 #include <SoftwareSerial.h>
 
+#define CHIP_SELECT 10
+
 SoftwareSerial InterArduino(7, 6); // RX, TX
+
 
 void setup() {
 	Serial.begin(9600);
 	Serial.println("Debut");
 
+	Serial.print("Initializing SD card...");
+	pinMode(10, OUTPUT);
+
+	if (!SD.begin(CHIP_SELECT)) {
+		Serial.println("Card failed, or not present");
+		return;
+	}
+	Serial.println("card initialized.");
+
 	InterArduino.begin(9600);
-	InterArduino.println("Debut Soft");
+	pinMode(5, OUTPUT);
+	pinMode(4, INPUT);
+	digitalWrite(5, LOW);
+
+	if(SD.exists("SAVE.JPG")) {
+		Serial.println("rm");
+		SD.remove("SAVE.JPG");
+	}
+
+	File saveFile = SD.open("SAVE.JPG", FILE_WRITE);
+
+	while(digitalRead(4) == LOW) {
+	}
+	Serial.println("Fin d'attente");
+	digitalWrite(5, HIGH);
+
+	if(saveFile) {
+		while (digitalRead(4) == HIGH) {
+			saveFile.print(InterArduino.read());
+			//Serial.println("X");
+		}
+		saveFile.close();
+	}
+	else
+		Serial.println("Echec d'ouverture de SAVE.JPG");
+
+  // re-open the file for reading:
+  saveFile = SD.open("SAVE.JPG");
+  if (saveFile) {
+    Serial.println("SAVE.JPG:");
+    // read from the file until there's nothing else in it:
+    while (saveFile.available()) {
+        Serial.write(saveFile.read());
+    }
+    // close the file:
+	delay(1000);
+	Serial.write(saveFile.size());
+    saveFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening SAVE.JPG for reading");
+  }
+	delay(2000);
+	digitalWrite(5, LOW);
 }
 
 void loop() {
-	InterArduino.println("Arduino2");
-	if (InterArduino.available())
-		Serial.write(InterArduino.read());
-	delay(1000);
+	Serial.println("Fin");
+	delay(5000);
 }
